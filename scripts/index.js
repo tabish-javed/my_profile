606ddeba1139dfba2e7deac4e2eb1d22bb40e154
TABISH <tabishjaved@hotmail.com>
2024-03-17 17:22:30 +0530

    experience heading fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,28 @@
 'use strict'
 
-const header = document.querySelector('header')
+const header = document.querySelector('.header')
 const navLinks = document.querySelectorAll('.nav-link')
 
 function onScroll () {
   window.addEventListener('scroll', () => {
     if (window.scrollY >= 2) {
       header.classList.add('scrolled')
-      navLinks.forEach(link => {
-        link.classList.add('nav-on-scroll')
-      })
+      if (navLinks) {
+        navLinks.forEach(link => {
+          link.classList.add('nav-on-scroll')
+        })
+      }
     } else {
       header.classList.remove('scrolled')
-      navLinks.forEach(link => {
-        link.classList.remove('nav-on-scroll')
-      })
+      if (navLinks) {
+        navLinks.forEach(link => {
+          link.classList.remove('nav-on-scroll')
+        })
+      }
     }
     // setTimeout(() => {
-    //   header.classList.add('hidden')
-    // }, 1000 * 2)
+    //   header.style.transform = 'translateY(-6rem)'
+    // }, 1000 * 3)
   })
 }
 
