db40f4048e287b92110a6238893de6c1b92907f5
TABISH <tabishjaved@hotmail.com>
2024-03-25 04:03:11 +0530

    accordion improved with logic

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,22 @@
 const header = document.querySelector('.header')
 const navLinks = document.querySelectorAll('.nav-link')
 const companies = document.querySelectorAll('.company-logo')
-const underlines = document.querySelectorAll('.underline')
+const companyRnRs = document.querySelectorAll('.company-rnr')
 
+// Helpers
+/**
+ * Get index of element in NodeList
+ * @param {Object} element - DOM element
+ * @param {Object} NodeList - NodeList of DOM elements
+ * @return {number} index of element in NodeList
+ */
+function getIndex (element, NodeList) {
+  return Array.from(NodeList).indexOf(element)
+}
 
+/**
+ * Activate Navbar on scroll
+ */
 function onScrollNavBar () {
   window.addEventListener('scroll', () => {
     if (window.scrollY >= 2) {
@@ -29,15 +42,19 @@ function onScrollNavBar () {
   })
 }
 
-
+/**
+ * Activate Company Accordion
+ */
 function experienceAccordion () {
   const companyLogos = document.querySelector('.company-logos')
   companyLogos.addEventListener('click', (e) => {
-    const companies = document.querySelectorAll('.company-logo')
+    const companyNumber = getIndex(e.target, companies)
 
     if (e.target.classList.contains('company-logo')) {
       companies.forEach(company => company.closest('div').classList.remove('company-active'))
       e.target.closest('div').classList.add('company-active')
+      companyRnRs.forEach(rnr => rnr.classList.add('disabled'))
+      companyRnRs[companyNumber].classList.remove('disabled')
     }
   })
 }
